875d3079dd6343a908d2512e355dee5c958d4d9e
openbusinessintelligence <openbusinessintelligence@gmail.com>
2014-04-23 18:31:03 +0200

    1

---
+++ openbiwiki-core/src/main/jflex/generate.bat
@@ -0,0 +1,8 @@
+D:\Programs\jflex-1.5.1\bin\jflex -d ../java/org/jamwiki/parser/jflex ^
+	jamwiki-custom-tag-processor.jflex ^
+	jamwiki-edit-comment-processor.jflex ^
+	jamwiki-html-processor.jflex ^
+	jamwiki-postprocessor.jflex ^
+	jamwiki-preprocessor.jflex ^
+	jamwiki-processor.jflex ^
+	jamwiki-spliceprocessor.jflex
---
+++ openbiwiki-core/src/main/jflex/generate_template.bat
@@ -0,0 +1,2 @@
+D:\Programs\jflex-1.5.1\bin\jflex -d ../java/org/jamwiki/parser/jflex ^
+	jamwiki-template-processor.jflex
